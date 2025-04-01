ee20578f88a5a06f90ba69d296c14ad12c20cd7f
osamabel <osamaalpha1997@gmail.com>
2025-04-01 23:48:15 +0000

    test

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,17 @@ const nextConfig: NextConfig = {
     images: {
         domains: [
             'template.creativemox.com',
+            'kc8w088c08808kwo0kcwc48c.64.226.67.120.sslip.io'
         ]
     },
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'kc8w088c08808kwo0kcwc48c.64.226.67.120.sslip.io',
+        port: '',
+        pathname: '/uploads/**',
+      },
+    ],
     async rewrites() {
         return [
           {
